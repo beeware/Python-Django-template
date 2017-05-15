70f0c5b45a2b968ab314b4f90c3f788db1f38ce3
Russell Keith-Magee <russell@keith-magee.com>
2017-05-15 09:28:17 +0800

    Revert app reference in URLs.

--- {{ cookiecutter.dir_name }}/briefcase/urls.py
+++ {{ cookiecutter.dir_name }}/briefcase/urls.py
@@ -24,7 +24,7 @@ from toga.django import TogaApp
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', TogaApp('{{ cookiecutter.app_name }}.app').urls)
+    url(r'', TogaApp('{{ cookiecutter.app_name }}').urls)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
